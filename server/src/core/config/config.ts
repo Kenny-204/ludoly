c2cd8dcfc4e1989bb7c72142bdf275d6d451b99d
Kehinde <giwatwins@gmail.com>
2026-04-26 16:36:07 +0100

    fix(config): update database connection string to use DB environment variable

--- server/src/core/config/config.ts
+++ server/src/core/config/config.ts
@@ -21,7 +21,7 @@ const config = Object.freeze({
   app: {
     port: Number(process.env.PORT) || 3000,
   },
-  db: { db_string: process.env.DB_LOCAL || "" },
+  db: { db_string: process.env.DB || "" },
   auth: {
     JWT_SECRET: process.env.JWT_SECRET || "",
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "",
